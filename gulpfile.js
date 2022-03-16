aa44fc8857030225aefa41a0f71dd4501845d97f
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-03-16 21:43:57 +0100

    [TEDEFO-712]: Added Gulp build script.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// see https://gitlab.com/antora/demo/docs-site/blob/watch-mode/gulpfile.js
+// see https://gitlab.com/antora/antora/-/issues/329
+'use strict';
+
+['http_proxy', 'https_proxy', 'no_proxy'].forEach((name) => {
+  if (process.env[name]) process.env[name.toUpperCase()] = process.env[name]
+})
+process.env.GLOBAL_AGENT_ENVIRONMENT_VARIABLE_NAMESPACE = ''
+require('global-agent/bootstrap')
+
+const connect = require('gulp-connect')
+const fs = require('fs')
+const generator = require('antora-eui-site-generator-lunr')
+process.env['DOCSEARCH_ENABLED'] = true
+process.env['DOCSEARCH_ENGINE'] = 'lunr'
+const { reload: livereload } = process.env.LIVERELOAD === 'true' ? require('gulp-connect') : {}
+const { series, src, watch } = require('gulp')
+const yaml = require('js-yaml')
+
+const playbookFilename = 'antora-playbook.yml'
+const playbook = yaml.safeLoad(fs.readFileSync(playbookFilename, 'utf8'))
+const outputDir = process.env['SITE_DIR'] || (playbook.output || {}).dir || 'build/site'
+const serverConfig = { name: 'Preview Site', livereload, port: 5000, root: outputDir }
+const antoraArgs = ['--playbook', playbookFilename, '--generator', 'antora-site-generator-lunr']
+const watchPatterns = playbook.content.sources.filter((source) => !source.url.includes(':')).reduce((accum, source) => {
+  accum.push(`${source.url}/${source.start_path ? source.start_path + '/' : ''}antora.yml`)
+  accum.push(`${source.url}/${source.start_path ? source.start_path + '/' : ''}**/*.adoc`)
+  return accum
+}, [])
+
+function generate (done) {
+  generator(antoraArgs, process.env)
+    .then(() => done())
+    .catch((err) => {
+      console.log(err)
+      done()
+    })
+}
+
+function serve (done) {
+  connect.server(serverConfig, function () {
+    this.server.on('close', done)
+    watch(watchPatterns, generate)
+    if (livereload) watch(this.root).on('change', (filepath) => src(filepath, { read: false }).pipe(livereload()))
+  })
+}
+
+module.exports = { serve, generate, default: generate }
